74c3e1cdb4ac57fa173ef206114869131450f590
Sadnur Islam <114425532+SadnurIslam@users.noreply.github.com>
2023-08-24 23:19:45 +0600

    Update display tree.cpp

--- display tree.cpp
+++ display tree.cpp
@@ -1,55 +1,40 @@
-
 #include<iostream>
 #include<math.h>
 using namespace std;
 
-int binTree[100] = {0};
-bool isOccupied[100] = {false};
-int insert(int data){
-	int pos = 0;
-	while(1){
-		if(!isOccupied[pos]){
-			binTree[pos] = data;
-			isOccupied[pos] = true;
-			return pos+1;
-		}
-		if(data == binTree[pos]){
-			cout<<"The number "<<data<<" already exists in the binary tree at position "<<pos+1<<"\n";
-			return pos+1;
-		}
-		else if(data < binTree[pos]){
-			pos = 2*pos + 1;
-		}
-		else{
-			pos = 2*pos + 2;
-		}
-	}
-}
-void displayTree(int tree[], int size){		//works better with single-digit nodes.
-	int i, j, space, lineGap, level = log2(size)+1;
-	for(i=0; i<level; i++){					//levels
-		for(j=0; j<(1<<i); j++){			//nodes of each level
-			space = (1<<(level-i))-1;
-			if(j == 0){
-				space /= 2;
+int tree[100],size;
+
+void displayTree(){		
+	int i, j, space, lineGap, level = log2(size+1);
+	for(i=0; i<=level; i++){					
+		for(j=0; j<(1<<i); j++){			
+			space = 2*(1<<(level-i));
+			if(j>0 && j%2==0){
+	                space=space/2;
 			}
 			while(space--){
 				cout<<" ";
 			}
-			cout<<tree[(1<<i)-1+j];
+			if(tree[(1<<i)+j-1]==-1){
+               			cout<<" ";
+			}
+			else
+			cout<<tree[(1<<i)+j-1];
+			space = 2*(1<<(level-i))-1;
+			while(space--){
+				cout<<" ";
+			}
 		}
-		for(lineGap=0; lineGap<(level-i-1); lineGap++){
+		for(lineGap=0; lineGap<(level-i); lineGap++){
 			cout<<"\n";
 		}
 	}
+	cout<<endl;
 }
 int main(){
-	int data[] = {8,3,10,1,6,-1,14,-1,-1,7};
-	int pos, mxSize = 0;
-	for(int i=0; i<10; i++){
-		pos = insert(data[i]);
-		mxSize = max(mxSize,pos);
-	}
-	displayTree(binTree, mxSize);
-	return 0;
+	tree[] ={3,5,1,2,7,9,4);
+	size = 7;
+	displayTree();
+
+return 0;
 }
